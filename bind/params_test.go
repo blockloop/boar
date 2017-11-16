07a434d355493def1d9a2a36fc21331206e8b7b1
Brett Jones <blockloop@users.noreply.github.com>
2017-11-15 20:46:34 -0600

    100% bind coverage ✊

--- bind/params_test.go
+++ bind/params_test.go
@@ -54,3 +54,58 @@ func TestParamsErrorsForArray(t *testing.T) {
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "array")
 }
+
+func TestParamsShouldSkipUnsettableFields(t *testing.T) {
+	var item struct {
+		unsetable int
+		Count     int
+	}
+
+	err := Params(&item, httprouter.Params{
+		{Key: "Count", Value: "1"},
+		{Key: "unsetable", Value: "1"},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 0, item.unsetable)
+	assert.Equal(t, 1, item.Count)
+}
+
+func TestParamsShouldSkipFieldsTaggedWithDash(t *testing.T) {
+	var item struct {
+		SkipMe int `url:"-"`
+		Count  int
+	}
+
+	err := Params(&item, httprouter.Params{
+		{Key: "Count", Value: "1"},
+		{Key: "SkipMe", Value: "1"},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 0, item.SkipMe)
+	assert.Equal(t, 1, item.Count)
+}
+
+func TestParamsShouldSkipFieldsWhenNoParamValue(t *testing.T) {
+	var item struct {
+		SkipMe int
+		Count  int
+	}
+
+	err := Params(&item, httprouter.Params{
+		{Key: "Count", Value: "1"},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 0, item.SkipMe)
+	assert.Equal(t, 1, item.Count)
+}
+
+func TestParamsShouldErrorWhenMismatchedType(t *testing.T) {
+	var item struct {
+		Count int
+	}
+
+	err := Params(&item, httprouter.Params{
+		{Key: "Count", Value: "abcd"},
+	})
+	assert.IsType(t, &TypeMismatchError{}, err)
+}
